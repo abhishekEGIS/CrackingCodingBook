b8cee9b06d6e83c143e86c8664f5eed0964d62d8
abhishekcabhyankar <abhishekcabhyankar@gmail.com>
2018-02-21 00:35:09 -0500

    Basic list printing

--- CrackingCodingBook/CrackingCodingBook.cpp
+++ CrackingCodingBook/CrackingCodingBook.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <list>
 
+using std::string;
+using std::list;
+using std::cout;
+using std::endl;
 
 int main()
 {
+
+	int selection;
+	list<string> strlist;
+
+	strlist.push_back(string("Problem 1"));
+	strlist.push_back(string("Problem 2"));
+	strlist.push_back(string("Problem 3"));
+
+	for (list<string>::iterator it = strlist.begin(); it != strlist.end(); ++it)
+	{
+		std::cout << (*it) << std::endl;
+	}
+
+	std::cin >> selection;
     return 0;
 }
 
